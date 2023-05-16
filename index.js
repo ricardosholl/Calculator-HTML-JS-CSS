288cb755b6fb999ed566c8900675abfa808d85fd
Ricardo Alves <88462852+ricardosholl@users.noreply.github.com>
2023-05-16 15:50:48 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,42 +1,6 @@
-function exibirNumero0() {
+function exibirNumero(novoNumero) {
   var numero = document.getElementById("mostrarExpressao");
-  numero.value += "0";
-}
-function exibirNumero1() {
-  var numero = document.getElementById("mostrarExpressao");
-  numero.value += "1";
-}
-function exibirNumero2() {
-  var numero = document.getElementById("mostrarExpressao");
-  numero.value += "2";
-}
-function exibirNumero3() {
-  var numero = document.getElementById("mostrarExpressao");
-  numero.value += "3";
-}
-function exibirNumero4() {
-  var numero = document.getElementById("mostrarExpressao");
-  numero.value += "4";
-}
-function exibirNumero5() {
-  var numero = document.getElementById("mostrarExpressao");
-  numero.value += "5";
-}
-function exibirNumero6() {
-  var numero = document.getElementById("mostrarExpressao");
-  numero.value += "6";
-}
-function exibirNumero7() {
-  var numero = document.getElementById("mostrarExpressao");
-  numero.value += "7";
-}
-function exibirNumero8() {
-  var numero = document.getElementById("mostrarExpressao");
-  numero.value += "8";
-}
-function exibirNumero9() {
-  var numero = document.getElementById("mostrarExpressao");
-  numero.value += "9";
+  numero.value += novoNumero;
 }
 function add() {
   var numero = document.getElementById("mostrarExpressao");
